3e851f1fd29af26e917353df3e111d150e3a0434
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-23 17:29:51 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,21 +1,21 @@
-create table projects(
+CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding_goal TEXT,
-  start_date date,
-  end_date date
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
 );
 
-create table users(
+ CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 );
 
-create table pledges(
+ CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
-  amount REAL,
+  amount INTEGER,
   user_id INTEGER,
   project_id INTEGER
 );
